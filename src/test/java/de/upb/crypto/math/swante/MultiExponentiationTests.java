e98da0110f8bbf79a2f8e05dfc6fa4fabd45f518
Swante Scholz <sscholz@mail.uni-paderborn.de>
2019-04-02 17:20:34 +0200

    first performance test working for orininal powproduct

--- src/test/java/de/upb/crypto/math/swante/MultiExponentiationTests.java
+++ src/test/java/de/upb/crypto/math/swante/MultiExponentiationTests.java
@@ -1,10 +1,9 @@
 package de.upb.crypto.math.swante;
 
-import de.upb.crypto.math.interfaces.structures.Field;
-import de.upb.crypto.math.interfaces.structures.GroupElement;
 import de.upb.crypto.math.interfaces.structures.PowProductExpression;
 import de.upb.crypto.math.structures.ec.AbstractEllipticCurvePoint;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigInteger;
@@ -12,7 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static de.upb.crypto.math.swante.MyExponentiationAlgorithms.*;
 import static de.upb.crypto.math.swante.misc.pln;
 
 public class MultiExponentiationTests {
@@ -22,33 +20,40 @@ public class MultiExponentiationTests {
     AbstractEllipticCurvePoint g = curve.getGenerator();
     
     int numBases = 10;
-    List<AbstractEllipticCurvePoint> bases = IntStream.range(0, numBases).mapToObj(it -> curve.getUniformlyRandomElement()).collect(Collectors.toList());
-    List<BigInteger> exponents = IntStream.range(0, numBases).mapToObj(it -> misc.randBig(parameters.p)).collect(Collectors.toList());
+    AbstractEllipticCurvePoint[] bases = IntStream.range(0, numBases).mapToObj(it -> curve.getUniformlyRandomElement()).collect(Collectors.toList()).toArray(new AbstractEllipticCurvePoint[0]);
+    BigInteger[] exponents = IntStream.range(0, numBases).mapToObj(it -> misc.randBig(parameters.p)).collect(Collectors.toList()).toArray(new BigInteger[0]);
+    PowProductExpression originalPowProductExpression = new PowProductExpression(curve);
+    
+    @Before
+    public void init() {
+        for (int i = 0; i < numBases; i++) {
+            AbstractEllipticCurvePoint base = bases[i];
+            BigInteger e = exponents[i];
+            originalPowProductExpression.op(base, e);
+        }
+    }
     
     @Test
     public void testCorrectness() {
         AbstractEllipticCurvePoint expected = curve.getNeutralElement();
-        PowProductExpression powProductExpression = new PowProductExpression(curve);
         for (int i = 0; i < numBases; i++) {
-            AbstractEllipticCurvePoint base = bases.get(i);
-            BigInteger e = exponents.get(i);
+            AbstractEllipticCurvePoint base = bases[i];
+            BigInteger e = exponents[i];
             expected = expected.op(base.pow(e));
-            powProductExpression.op(base, e);
         }
-        Assert.assertEquals(expected, curve.evaluate(powProductExpression));
+        Assert.assertEquals(expected, curve.evaluate(originalPowProductExpression));
     }
     
     @Test
     public void testPerformance() {
-        int numIterations = 10;
+        int numIterations = 20;
         
         
         pln("==========================");
-//        pln(String.format("wsize=%d (m=%d), #bases=%d, #exponents=%d", windowSize, m, numBases, numExponents));
+        pln(String.format("#iterations=%d, #bases=%d", numIterations, numBases));
         misc.tick();
         for (int i = 0; i < numIterations; i++) {
-            AbstractEllipticCurvePoint base = bases.get(i);
-            
+            curve.evaluate(originalPowProductExpression);
         }
         pln(String.format("normal pow -> %.2f ms", misc.tick()));
         misc.tick();
