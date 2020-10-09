3337b3cf4425b12043f97269beb877d41a22ce29
raphael <rheitjoh@mail.uni-paderborn.de>
2020-10-09 11:14:19 +0200

    Delete old manual tests and compare sliding A1 and A2

--- src/main/java/de/upb/crypto/math/structures/groups/exp/ExponentiationAlgorithms.java
+++ src/main/java/de/upb/crypto/math/structures/groups/exp/ExponentiationAlgorithms.java
@@ -4,8 +4,7 @@ import de.upb.crypto.math.interfaces.structures.group.impl.GroupElementImpl;
 import de.upb.crypto.math.interfaces.structures.group.impl.GroupImpl;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * A class containing multi-exponentiation algorithms.
@@ -135,11 +134,12 @@ public class ExponentiationAlgorithms {
      * for a large amount of bases, the precomputation is a lot cheaper than in the simultaneous
      * approach.
      */
-    public static GroupElementImpl interleavingSlidingWindowMultiExp(Multiexponentiation multiexp, int windowSize) {
-        multiexp.ensurePrecomputation(windowSize);
-        List<MultiExpTerm> terms = multiexp.getTerms();
+    public static GroupElementImpl interleavingSlidingWindowMultiExpA1(Multiexponentiation multiexp, int windowSize) {
+        Multiexponentiation newMultiexp = convertToNonNegativeExponents(multiexp);
+        List<MultiExpTerm> terms = newMultiexp.getTerms();
+        newMultiexp.ensurePrecomputation(windowSize);
         if (terms.isEmpty()) //nothing to do here.
-            return multiexp.getConstantFactor().orElseThrow(() -> new IllegalArgumentException("Cannot compute an empty multiexp"));
+            return newMultiexp.getConstantFactor().orElseThrow(() -> new IllegalArgumentException("Cannot compute an empty multiexp"));
         int numTerms = terms.size();
 
         // we are assuming that every base has same underlying group
@@ -165,19 +165,104 @@ public class ExponentiationAlgorithms {
                     for (int k = j; k >= J; k--) {
                         windowVal[i] <<= 1;
                         if (testBit(exponent, k)) {
-                            windowVal[i]++; //TODO e[i] = e[i] | 1 ?
+                            windowVal[i]++; // TODO e[i] = e[i] | 1 ?
                         }
                     }
                 } //now wait for the window position to occur through the squaring steps
                 if (windowPos[i] == j) { //found window position. Multiply the whole thing with base^windowVal
-                    result = result.op(terms.get(i).getPrecomputation().get(windowVal[i])); //TODO was: oddPowers.get(i).get(windowVal[i] / 2)
+                    result = result.op(terms.get(i).getPrecomputation().get(windowVal[i]));
                     windowPos[i] = -1;
                 }
             }
         }
 
         //Multiply with constant specified in the Multiexponentiation
-        result = multiexp.getConstantFactor().map(result::op).orElse(result);
+        result = newMultiexp.getConstantFactor().map(result::op).orElse(result);
+
+        return result;
+    }
+
+    /**
+     * Convert every negative exponent in the multiexponentiation by inverting the base instead.
+     * @param multiexp
+     * @return
+     */
+    public static Multiexponentiation convertToNonNegativeExponents(Multiexponentiation  multiexp) {
+        List<MultiExpTerm> terms = multiexp.getTerms();
+        Multiexponentiation newMultiexp = new Multiexponentiation();
+        newMultiexp.put(multiexp.getConstantFactor().orElse(null));
+        // Invert base of every term that has negative exponent
+        for (MultiExpTerm term : terms) {
+            if (term.getExponent().signum() < 0) {
+                newMultiexp.put(new MultiExpTerm(term.getBase().inv(), term.getExponent().negate()));
+            } else {
+                newMultiexp.put(term);
+            }
+        }
+        return newMultiexp;
+    }
+
+    public static GroupElementImpl interleavingSlidingWindowMultiExpA2(Multiexponentiation multiexp, int windowSize) {
+        List<MultiExpTerm> terms = multiexp.getTerms();
+        // Ensure every exponent is positive and store the ones that were negative
+        Set<Integer> negTermIndices = new HashSet<>();
+        Multiexponentiation newMultiexp = new Multiexponentiation();
+        newMultiexp.put(multiexp.getConstantFactor().orElse(null));
+        for (int i = 0; i < terms.size(); ++i) {
+            MultiExpTerm term = terms.get(i);
+            if (term.getExponent().signum() < 0) {
+                newMultiexp.put(new MultiExpTerm(term.getBase(), term.getExponent().negate(), term.getPrecomputation()));
+                negTermIndices.add(i);
+            } else {
+                newMultiexp.put(term);
+            }
+        }
+        terms = newMultiexp.getTerms();
+        newMultiexp.ensurePrecomputation(windowSize);
+        if (terms.isEmpty()) //nothing to do here.
+            return newMultiexp.getConstantFactor().orElseThrow(() -> new IllegalArgumentException("Cannot compute an empty multiexp"));
+        int numTerms = terms.size();
+
+        // we are assuming that every base has same underlying group
+        GroupElementImpl result = terms.get(0).getBase().getStructure().getNeutralElement();
+        int longestExponentBitLength = terms.stream().mapToInt(t -> t.getExponent().bitLength()).max().getAsInt();
+        int[] windowPos = new int[numTerms]; //position of the (sliding) window for each base. -1 signifies next window position must be computed.
+        Arrays.fill(windowPos, -1);
+        int[] windowVal = new int[numTerms]; //the exponent of the current window.
+
+        for (int j = longestExponentBitLength - 1; j >= 0; j--) {
+            if (j != longestExponentBitLength - 1) {
+                result = result.square();
+            }
+            for (int i = 0; i < numTerms; i++) { //for each term
+                BigInteger exponent = terms.get(i).getExponent();
+                if (windowPos[i] == -1 && exponent.testBit(j)) { //start a new window
+                    int J = j - windowSize + 1;
+                    while (!testBit(exponent, J)) {
+                        J++;
+                    }
+                    windowPos[i] = J;
+                    windowVal[i] = 0;
+                    for (int k = j; k >= J; k--) {
+                        windowVal[i] <<= 1;
+                        if (testBit(exponent, k)) {
+                            windowVal[i]++; // TODO e[i] = e[i] | 1 ?
+                        }
+                    }
+                } //now wait for the window position to occur through the squaring steps
+                if (windowPos[i] == j) { //found window position. Multiply the whole thing with base^windowVal
+                    if (negTermIndices.contains(i)) {
+                        result = result.op(terms.get(i).getPrecomputation().get(windowVal[i]).inv());
+                    } else {
+                        result = result.op(terms.get(i).getPrecomputation().get(windowVal[i]));
+                    }
+                    windowPos[i] = -1;
+                }
+            }
+        }
+
+        //Multiply with constant specified in the Multiexponentiation
+        result = newMultiexp.getConstantFactor().map(result::op).orElse(result);
 
         return result;
     }
@@ -262,108 +347,10 @@ public class ExponentiationAlgorithms {
         return result;
     }
 
-    public static GroupElementImpl slidingWindowExpA1(GroupElementImpl base, BigInteger exponent,
-                                                      SmallExponentPrecomputation precomputation, int windowSize) {
+    public static GroupElementImpl slidingWindowExp(GroupElementImpl base, BigInteger exponent,
+                                                    SmallExponentPrecomputation precomputation, int windowSize) {
         if (exponent.signum() < 0) {
-            return slidingWindowExpA1(base.inv(), exponent.negate(), null, windowSize);
-        }
-        if (precomputation == null)
-            precomputation = new SmallExponentPrecomputation(base);
-        else
-            windowSize = Math.max(precomputation.getCurrentSupportedWindowSize(), windowSize);
-
-        precomputation.compute(windowSize);
-
-        // we are assuming that every base has same underlying group
-        GroupElementImpl result = base.getStructure().getNeutralElement();
-        int exponentBitlen = exponent.bitLength(); //TODO maybe skip this and always set it to log p?
-        int windowPos = -1; //position of the (sliding) window. -1 signifies next window position must be computed.
-        int windowVal = 0; //the exponent of the current window.
-
-        for (int j = exponentBitlen - 1; j >= 0; j--) {
-            if (j != exponentBitlen - 1) {
-                result = result.square();
-            }
-
-            if (windowPos == -1 && exponent.testBit(j)) { //start a new window
-                int J = j - windowSize + 1;
-                while (!testBit(exponent, J)) {
-                    J++;
-                }
-                windowPos = J;
-                windowVal = 0;
-                for (int k = j; k >= J; k--) {
-                    windowVal <<= 1;
-                    if (testBit(exponent, k)) {
-                        windowVal++;
-                    }
-                }
-            } //now wait for the window position to occur through the squaring steps
-            if (windowPos == j) { //found window position. Multiply the whole thing with base^windowVal
-                result = result.op(precomputation.get(windowVal));
-                windowPos = -1;
-            }
-        }
-
-        return result;
-    }
-
-    public static GroupElementImpl slidingWindowExpA2(GroupElementImpl base, BigInteger exponent,
-                                                      SmallExponentPrecomputation precomputation, int windowSize) {
-        BigInteger actualExponent = exponent;
-        boolean isExponentNegative = false;
-        if (exponent.signum() < 0) {
-            isExponentNegative = (exponent.signum() < 0);
-            actualExponent = exponent.negate();
-        }
-        if (precomputation == null)
-            precomputation = new SmallExponentPrecomputation(base);
-        else
-            windowSize = Math.max(precomputation.getCurrentSupportedWindowSize(), windowSize);
-
-        precomputation.compute(windowSize);
-
-        // we are assuming that every base has same underlying group
-        GroupElementImpl result = base.getStructure().getNeutralElement();
-        int exponentBitlen = actualExponent.bitLength(); //TODO maybe skip this and always set it to log p?
-        int windowPos = -1; //position of the (sliding) window. -1 signifies next window position must be computed.
-        int windowVal = 0; //the exponent of the current window.
-
-        for (int j = exponentBitlen - 1; j >= 0; j--) {
-            if (j != exponentBitlen - 1) {
-                result = result.square();
-            }
-
-            if (windowPos == -1 && actualExponent.testBit(j)) { //start a new window
-                int J = j - windowSize + 1;
-                while (!testBit(actualExponent, J)) {
-                    J++;
-                }
-                windowPos = J;
-                windowVal = 0;
-                for (int k = j; k >= J; k--) {
-                    windowVal <<= 1;
-                    if (testBit(actualExponent, k)) {
-                        windowVal++;
-                    }
-                }
-            } //now wait for the window position to occur through the squaring steps
-            if (windowPos == j) { //found window position. Multiply the whole thing with base^windowVal
-                if (isExponentNegative)
-                    result = result.op(precomputation.get(windowVal).inv());
-                else
-                    result = result.op(precomputation.get(windowVal));
-                windowPos = -1;
-            }
-        }
-
-        return result;
-    }
-
-    public static GroupElementImpl slidingWindowExpA3(GroupElementImpl base, BigInteger exponent,
-                                                      SmallExponentPrecomputation precomputation, int windowSize) {
-        if (exponent.signum() < 0) {
-            return slidingWindowExpA1(base, exponent.negate(), null, windowSize).inv();
+            return slidingWindowExp(base, exponent.negate(), precomputation, windowSize).inv();
         }
         if (precomputation == null)
             precomputation = new SmallExponentPrecomputation(base);
--- src/test/java/de/upb/crypto/math/test/ManualTest.java
+++ src/test/java/de/upb/crypto/math/test/ManualTest.java
@@ -3,6 +3,7 @@ package de.upb.crypto.math.test;
 import de.upb.crypto.math.factory.BilinearGroup;
 import de.upb.crypto.math.factory.BilinearGroupFactory;
 import de.upb.crypto.math.factory.BilinearGroupRequirement;
+import de.upb.crypto.math.interfaces.structures.Group;
 import de.upb.crypto.math.interfaces.structures.GroupElement;
 import de.upb.crypto.math.interfaces.structures.RingGroup;
 import de.upb.crypto.math.interfaces.structures.group.impl.GroupElementImpl;
@@ -12,12 +13,16 @@ import de.upb.crypto.math.pairings.debug.count.CountingGroup;
 import de.upb.crypto.math.structures.groups.basic.BasicGroup;
 import de.upb.crypto.math.structures.groups.basic.BasicGroupElement;
 import de.upb.crypto.math.structures.groups.exp.ExponentiationAlgorithms;
+import de.upb.crypto.math.structures.groups.exp.MultiExpTerm;
+import de.upb.crypto.math.structures.groups.exp.Multiexponentiation;
 import de.upb.crypto.math.structures.groups.lazy.LazyGroupElement;
 import de.upb.crypto.math.structures.zn.Zn;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,24 +30,34 @@ public class ManualTest {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         BilinearGroupFactory fac = new BilinearGroupFactory(60);
         fac.setRequirements(BilinearGroup.Type.TYPE_3);
+
+        BilinearGroup bilGroup = fac.createBilinearGroup();
+        for (int i = 0; i < 10; ++i) {
+            Multiexponentiation multiexponentiation = genMultiExp(bilGroup.getG1(), 10);
+            assertEquals(ExponentiationAlgorithms.interleavingSlidingWindowMultiExpA1(multiexponentiation, 4),
+                    ExponentiationAlgorithms.interleavingSlidingWindowMultiExpA2(multiexponentiation, 4));
+            assertEquals(ExponentiationAlgorithms.interleavingWnafMultiExp(multiexponentiation, 4),
+                    ExponentiationAlgorithms.interleavingSlidingWindowMultiExpA1(multiexponentiation, 4));
+            assertEquals(ExponentiationAlgorithms.interleavingWnafMultiExp(multiexponentiation, 4),
+                    ExponentiationAlgorithms.interleavingSlidingWindowMultiExpA2(multiexponentiation, 4));
+        }
+    }
+
+    public static Multiexponentiation genMultiExp(Group group, int numTerms) throws NoSuchMethodException,
+            InvocationTargetException, IllegalAccessException {
+        SecureRandom secRand = new SecureRandom();
+        Random rand = new Random(secRand.nextLong());
+        Multiexponentiation multiexponentiation = new Multiexponentiation();
         Method getValue = LazyGroupElement.class.getDeclaredMethod("getConcreteValue");
         getValue.setAccessible(true);
-        BilinearGroup bilGroup = fac.createBilinearGroup();
-        for (int i = -500; i < 500; ++i) {
-            GroupElementImpl elem = (GroupElementImpl) getValue.invoke(bilGroup.getG1().getUniformlyRandomElement());
-            GroupElementImpl correctResult = ExponentiationAlgorithms.binSquareMultiplyExp(elem, BigInteger.valueOf(i));
-            assertEquals(
-                    ExponentiationAlgorithms.slidingWindowExpA1(elem, BigInteger.valueOf(i), null, 4),
-                    correctResult
-            );
-            assertEquals(
-                    ExponentiationAlgorithms.slidingWindowExpA2(elem, BigInteger.valueOf(i), null, 4),
-                    correctResult
-            );
-            assertEquals(
-                    ExponentiationAlgorithms.slidingWindowExpA3(elem, BigInteger.valueOf(i), null, 4),
-                    correctResult
+        for (int i = 0; i < numTerms; ++i) {
+            multiexponentiation.put(
+                    new MultiExpTerm(
+                            (GroupElementImpl) getValue.invoke(group.getUniformlyRandomNonNeutral()),
+                            BigInteger.valueOf(rand.nextLong())
+                    )
             );
         }
+        return multiexponentiation;
     }
 }
