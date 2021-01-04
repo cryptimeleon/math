cbc9409bcfadbdb389460c2264943d979682c6a1
raphael <rheitjoh@mail.uni-paderborn.de>
2021-01-04 10:28:22 +0100

    Add lazy wrapper classes for bilinear group impls and move performance testing classes to benchmark project

--- src/test/java/de/upb/crypto/math/test/ManualTest.java
+++ src/test/java/de/upb/crypto/math/test/ManualTest.java
@@ -1,5 +1,7 @@
 package de.upb.crypto.math.test;
 
+import de.upb.crypto.math.pairings.generic.BilinearGroup;
+import de.upb.crypto.math.pairings.type3.bn.BarretoNaehrigBilinearGroup;
 import de.upb.crypto.math.structures.groups.exp.ExponentiationAlgorithms;
 
 import java.math.BigInteger;
