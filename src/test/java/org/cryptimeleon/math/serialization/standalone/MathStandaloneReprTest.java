216f9459345f587c67e7b5cd0e7223c0528b7d19
Jan Bobolz <jan.bobolz@uni-paderborn.de>
2021-02-27 12:04:34 +0100

    Improved reuse for StandaloneReprTests
    No need for copy/paste for derived packages anymore

---
+++ src/test/java/org/cryptimeleon/math/serialization/standalone/MathStandaloneReprTest.java
@@ -0,0 +1,7 @@
+package org.cryptimeleon.math.serialization.standalone;
+
+public class MathStandaloneReprTest extends StandaloneReprTest {
+    public MathStandaloneReprTest() {
+        super("org.cryptimeleon.math");
+    }
+}
